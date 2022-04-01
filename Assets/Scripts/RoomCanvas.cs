2ef20a5b8748f7a7cd1b9752dd52cc1e4cf274c1
Dreamcatcher <jamesaoki7@gmail.com>
2022-04-01 13:46:23 +0900

    ready test

--- Assets/Scripts/RoomCanvas.cs
+++ Assets/Scripts/RoomCanvas.cs
@@ -3,6 +3,25 @@ using UnityEngine.UI;
 
 public class RoomCanvas: MonoBehaviour {
 
+
+	[HideInInspector]
+	public string selectedHero;
+	private PhotonView PV;
+	private bool PlayerReady = false;
+	private ExitGames.Client.Photon.Hashtable _playerCustomProperties = new ExitGames.Client.Photon.Hashtable();
+
+	// private bool AllPlayersReady
+	// {
+	// 	get
+	// 	{
+	// 		foreach (var photonPlayer in PhotonNetwork.playerList)
+	// 		{
+	// 			if(photonPlayer.CustomProperties["Ping"] == false) return false;
+	// 		}
+
+	// 		return true;
+	// 	}
+	// }
 	public string character = "";
 
 	public void OnStartMatch() {
@@ -23,5 +42,35 @@ public class RoomCanvas: MonoBehaviour {
 	public void Chicken() {
 		PlayerNetwork.Instance.cha = "ChickenM";
 	}
+
+	
+
+     private void Update()
+     {
+         Debug.Log("Player Ready = " + _playerCustomProperties["PlayerReady"]);
+     }
+
+     private void Ready()
+     {
+         PlayerReady = true;    
+         selectedHero = "PlayerTest";
+         PhotonNetwork.SetPlayerCustomProperties(_playerCustomProperties);
+         _playerCustomProperties["PlayerReady"] = PlayerReady;
+     }
+
+     public void OnReady()
+     {
+         Ready();
+
+        //  if (PhotonNetwork.IsMasterClient)
+        //  {
+        //      foreach (var photonPlayer in PhotonNetwork.PlayerList)
+        //      {
+        //          photonPlayer.CustomProperties["PlayerReady"] = true;
+        //          PhotonNetwork.LoadLevel(3);
+        //      }
+        //  }
+
+     }
 
 }
