d178d91425b82eb4ebed339c1fa089bea8987e1a
Dreamcatcher <jamesaoki7@gmail.com>
2022-04-03 17:59:26 +0900

    add image

--- Assets/Scripts/CreateRoom.cs
+++ Assets/Scripts/CreateRoom.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Runtime.InteropServices;
+
 
 public class CreateRoom: MonoBehaviour {
 
